f7cf3dbf745e2dfa22b895782dc95208506837a0
Aniket Sharma <aniketsharma2147@gmail.com>
2021-05-01 12:08:17 +0530

    play again function

--- components/Result.js
+++ components/Result.js
@@ -3,35 +3,42 @@ import {Button, View, StyleSheet, Text} from 'react-native';
 
 const Result = ({score, playAgain}) => (
   <View style={styles.scoreBoard}>
-    <View style={styles.score}>
-      <Text>You scored {score}/5 correct answers!</Text>
+    <View>
+      <Text style={styles.score}>You scored {score}/5 correct answers!</Text>
     </View>
-    <Button style={styles.playBtn} onPress={playAgain} color="limegreen" title="Play Again!">
-    </Button>
+    <View style={styles.playBtn}>
+      <Button
+        onPress={playAgain}
+        color="limegreen"
+        title="Play Again!"></Button>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   scoreBoard: {
+    marginTop: 30,
     position: 'relative',
-    height: 700,
+    height: 400,
+    borderStyle: 'solid',
+    borderColor : 'black',
+    borderWidth : 1.5,
+    shadowColor:'grey',
   },
   score: {
     position: 'absolute',
-    fontSize: 7,
-    top: 150,
-    textAlign: 'center',
+    fontSize: 21,
+    top: 100,
+    alignSelf: 'center',
   },
   playBtn: {
     position: 'absolute',
-    fontSize: 1.4,
-    padding: 10,
-    marginTop: 150 ,
-    width: 110,
-    left: 0,
-    right: 0,
+    padding: 20,
+    top: 200,
+    width: 310,
     margin: 'auto',
-  }
-})
+    alignSelf: 'center',
+  },
+});
 
 export default Result;
